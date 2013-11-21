c4b22395a79be4eb01c862a51f5fd94a72537776
Roland <rolisz@yahoo.com>
2013-11-21 22:54:36 +0200

    Init

---
+++ installer.ps1
@@ -0,0 +1,21 @@
+#Invoke-Environment 'call installChocolatey.cmd'
+#iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+cinst vlc
+cinst notepadplusplus.install
+cinst notepadreplacer
+cinst git.install
+cinst 7zip.install
+cinst filezilla
+cinst skype
+cinst dropbox
+cinst windirstat
+cinst vim
+cinst calibre
+cinst FoxitReader
+cinst teamviewer
+cinst SQLite
+cinst f.lux
+cinst KatMouse
+cinst SourceCodePro
+cinst Gow
+scoop install python27 python pshazz concfg 7zip openssh
